44f223fded303d4b131d2d641711b84a63c0f668
Abe <abhi.rawat456@gmail.com>
2020-10-07 15:38:00 +0530

    Profile Screen Ready

--- Restauro/Profile/ProfileScreen.swift
+++ Restauro/Profile/ProfileScreen.swift
@@ -10,7 +10,69 @@ import SwiftUI
 
 struct ProfileScreen: View {
     var body: some View {
-        Text("Profile")
+        NavigationView {
+            ScrollView {
+                Image("person")
+                    .resizable()
+                    .clipped()
+                    .scaledToFit()
+                    .cornerRadius(100)
+                    .frame(height: 200)
+                    .padding(.vertical, 20)
+                
+                Text("Hi, dude").font(.title)
+                
+                
+                NavigationLink(destination : Text("Hi")) {
+                    HStack {
+                        Text("Change Name")
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                    }.foregroundColor(.black)
+                }
+                .padding(.vertical, 10)
+                NavigationLink(destination : Text("Hi")) {
+                    HStack {
+                        Text("Change Password")
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                    }.foregroundColor(.black)
+
+                }
+                .padding(.vertical, 10)
+                NavigationLink(destination : Text("Hi")) {
+                    HStack {
+                        Text("App Settings")
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                    }.foregroundColor(.black)
+
+                }
+                .padding(.vertical, 10)
+                NavigationLink(destination : Text("Hi")) {
+                    HStack {
+                        Text("Help and Support")
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                    }.foregroundColor(.black)
+
+                }
+                .padding(.vertical, 10)
+                
+                ZStack {
+                    Color.red.frame(height: 50).cornerRadius(5)
+                    Text("Sign out").foregroundColor(.white).bold()
+                }.padding(.vertical, 10)
+                .onTapGesture(perform: handleSignout)
+                
+            }
+            .padding(.horizontal, 15)
+            .navigationBarTitle("Profile")
+        }
+    }
+    
+    func handleSignout() {
+        print("Sign out")
     }
 }
 
